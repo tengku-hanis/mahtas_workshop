3d9bedd211adc538e2557c97c2c93f4fa09ded25
tengku-hanis <tengkuhanismokhtar@gmail.com>
2026-07-21 19:46:18 +0800

    initial commit

---
+++ Sesi 2/R scripts/1_packages.R
@@ -0,0 +1,9 @@
+# Install packages 
+# July 21, 2026
+
+
+
+# Install necessary package -----------------------------------------------
+
+pckg <- c("tidyverse", "naniar", "summarytools", "skimr")
+install.packages(pckg, dependencies = TRUE)
